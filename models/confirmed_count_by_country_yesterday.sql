dd3e6ada978738780f8fef0c1b4f9a5cd221fbdc
Mahangu <mahangu@automattic.com>
2020-04-28 19:31:19 +0530

    Added dtb_utils date macros to generate data for the current date minus 1 day.

--- models/confirmed_count_by_country_yesterday.sql
+++ models/confirmed_count_by_country_yesterday.sql
@@ -1,9 +1,10 @@
 select 
 		summary.country_region as country_name,
-		sum(summary.confirmed) as confirmed_count
+		sum(summary.confirmed) as confirmed_count,
+		summary.date as date
 	
 	from proven-now-275605.dbt_mahangu.summary
-  
-	where summary.date = '2020-04-25'
+ 
+	where summary.date = cast ( {{ dbt_utils.dateadd(datepart='day', interval=-1, from_date_or_timestamp=dbt_utils.current_timestamp() ) }} as date)
 	  
-	group by country_name
+	group by country_name, date
